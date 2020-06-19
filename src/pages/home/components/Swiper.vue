0532ec22304f026dce995af8bc77eec8618eba2d
Sun_chan <1291931321@qq.com>
2020-06-19 09:37:53 +0800

    change

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,12 +1,17 @@
+  
 <template>
 <div class="wrapper">
   <swiper :options="swiperOptions">
-    <swiper-slide v-for="item of swiperList" :key="item.id">
-      <img class="swiper-img" :src="item.imgUrl"/>
+    <swiper-slide >
+      <img class="swiper-img" src="../../../assets/images/picEight.jpg"/>
+    </swiper-slide>
+    <swiper-slide >
+      <img class="swiper-img" src="../../../assets/images/picNine.jpg"/>
     </swiper-slide>
     <div class="swiper-pagination" slot="pagination"></div>
   </swiper>
 </div>
+
 </template>
 
 <script>
@@ -15,30 +20,18 @@ export default{
   data () {
     return {
       swiperOptions: {
-        pagination: '.swiper-pagination',
-        lppo: true
-      },
-      swiperList: [{
-        id: '0001',
-        imgUrl: '../../../assets/images/picEight.jpg'
-      }, {
-        id: '0002',
-        imgUrl: '../../../assets/images/picNine.jpg'
-      }]
+        pagination: '.swiper-pagination'
+      }
     }
   }
 }
 </script>
 
-<style lang="stylus" scoped>
-  .wrapper >>> .swiper-pagination-bullet-active
-      background:#fff
+<style lang="stylus" scope>
   .wrapper
     overflow:hidden
     width:100%
-    height:0
-    padding-bottom:31.25%
-    background:#eee
+    background: #eee
     .swiper-img
       width:100%
 </style>
